e92d9ea4f725988acb8bb3d3c050531973183461
miruliana <miruliana2012@yahoo.com>
2014-01-20 14:15:48 +0200

    test2

---
+++ DemoShop/ShopDALRepository/Repositories/CosmeticRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopDAL.Model;
+using ShopDAL.Repository;
+using ShopDALRepository.Interfaces;
+
+namespace ShopDALRepository.Repositories
+{
+	public partial class CosmeticRepository : Repository<Cosmetic>, ICosmeticRepository
+	{
+		#region Ctors
+
+		public CosmeticRepository(ShopContext context)
+			: base(context)
+		{
+		}
+		public string GetName(object id)
+		{
+			Cosmetic entity = base.GetById(id);
+			return entity.Name;
+		}
+		public double GetPrice(object id)
+		{
+			Cosmetic entity = base.GetById(id);
+			return entity.Price;
+		}
+		public DateTime? GetExpireDate(object id)
+		{
+			Cosmetic entity = base.GetById(id);
+			return entity.ExpireDate;
+		}
+		public DateTime? GetFabricationDate(object id)
+		{
+			Cosmetic entity = base.GetById(id);
+			return entity.FabricationDate;
+		}
+		#endregion Ctors
+	}
+}
---
+++ DemoShop/ShopDALRepository/Repositories/Repository.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ShopDAL.Model;
+using ShopDALRepository.Interfaces;
+
+namespace ShopDAL.Repository
+{
+	public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+	{
+		protected ShopContext context;
+		protected DbSet<TEntity> dbSet;
+
+		public Repository(ShopContext context)
+		{
+			this.context = context;
+			this.dbSet = context.Set<TEntity>();
+		}
+
+		public IQueryable<TEntity> Entities
+		{
+			get { return dbSet; }
+		}
+
+		public virtual TEntity GetById(object id)
+		{
+			return dbSet.Find(id);
+		}
+
+		public virtual IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> filter = null)
+		{
+			IQueryable<TEntity> query = dbSet;
+
+			if (filter != null)
+			{
+				query = query.Where(filter);
+			}
+			return query;
+		}
+
+		public virtual IQueryable<TEntity> Get(
+		   Expression<Func<TEntity, bool>> filter = null,
+		   Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+		   string includeProperties = "")
+		{
+			IQueryable<TEntity> query = dbSet;
+
+			if (filter != null)
+			{
+				query = query.Where(filter);
+			}
+
+			foreach (var includeProperty in includeProperties.Split
+				(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+			{
+				query = query.Include(includeProperty);
+			}
+
+			if (orderBy != null)
+			{
+				return orderBy(query);
+			}
+			else
+			{
+				return query;
+			}
+		}
+
+
+		public virtual void Insert(TEntity entity)
+		{
+			dbSet.Add(entity);
+		}
+
+		public virtual void Delete(object id)
+		{
+			TEntity entityToDelete = dbSet.Find(id);
+			Delete(entityToDelete);
+		}
+
+		public virtual void Delete(TEntity entityToDelete)
+		{
+			if (context.Entry(entityToDelete).State == EntityState.Detached)
+			{
+				dbSet.Attach(entityToDelete);
+			}
+			dbSet.Remove(entityToDelete);
+		}
+
+		public virtual void Update(TEntity entityToUpdate)
+		{
+			dbSet.Attach(entityToUpdate);
+			context.Entry(entityToUpdate).State = EntityState.Modified;
+		}
+
+		private bool disposed = false;
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!this.disposed)
+			{
+				if (disposing)
+				{
+					context.Dispose();
+				}
+			}
+			this.disposed = true;
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+	}
+}
