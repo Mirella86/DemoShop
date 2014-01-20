55c80d917c7d7e11b6851d88d0dda72324a1fd2f
miruliana <miruliana2012@yahoo.com>
2014-01-20 14:25:56 +0200

    tt

---
+++ DemoShop/ShopUI/WindsorDependencyResolver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Dependencies;
+using Castle.Windsor;
+
+namespace ShopUI
+{
+	//public class WindsorDependencyResolver : IDependencyResolver
+	//{
+	//	private readonly IWindsorContainer _container;
+
+	//	public WindsorDependencyResolver(IWindsorContainer container)
+	//	{
+	//		_container = container;
+	//	}
+	//	public IDependencyScope BeginScope()
+	//	{
+	//		throw new NotImplementedException();
+	//	}
+
+	//	//public object GetService(Type serviceType)
+	//	//{
+	//	// if (_container.Kernel.HasComponent(serviceType)
+	//	//  return this._container.Resolve(serviceType);
+	//	// else
+	//	//  return null;
+	//	//}
+
+	//	public IEnumerable<object> GetServices(Type serviceType)
+	//	{
+	//		throw new NotImplementedException();
+	//	}
+
+	//	public void Dispose()
+	//	{
+	//		throw new NotImplementedException();
+	//	}
+	//}
+}
